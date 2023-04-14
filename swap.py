34f6d7611df1c7547eaf0ce96f89d9031f8d83b4
Krishna Jangid <108576349+Krishnaa234@users.noreply.github.com>
2023-04-14 23:11:32 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,10 @@
+x = input("Value of x: ")
+y = input("Valur of y: ")
+
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+
+x, y = y, x
+
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
