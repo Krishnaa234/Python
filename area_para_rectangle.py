1caaa84a221c2bfae7b3c004362c534da63a58b1
Krishna Jangid <108576349+Krishnaa234@users.noreply.github.com>
2023-04-21 01:39:46 +0530

    Add files via upload

---
+++ area_para_rectangle.py
@@ -0,0 +1,6 @@
+leng = float(input("Enter the length: "))
+bre = float(input("Enter the breadth: "))
+area = leng * bre
+para = 2 * (leng + bre)
+print("The area of the rectangle is: ", area)
+print("The parameter of the rectangle is: ", para)
